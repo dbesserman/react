327638d55d5c6472c17bea40b861bda7635897e2
David Besserman <david.besserman@gmail.com>
2018-09-28 12:01:36 +0200

    save

---
+++ ValidationComponent.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class ValidationComponent extends Component {
+  render() {
+    return (<div></div>);
+  }
+}
+export default ValidationComponent;
---
+++ src/ValidationComponent/ValidationComponent.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'component';
+
+class ValidationComponent extends Component {
+  render() {
+    return <div></div>
+  }
+}
+export default ValidationComponent;
